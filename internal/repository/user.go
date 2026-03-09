4dba3c689d463ea8d033bef84f078ec48897a951
mslthn <msulthon1909@gmail.com>
2026-03-09 16:44:15 +0700

    feat: create user repository

--- internal/repository/user.go
+++ internal/repository/user.go
@@ -1,8 +1,72 @@
 package repository
 
-import "github.com/jackc/pgx/v5"
+import (
+	"context"
+	"main/internal/models"
+
+	"github.com/jackc/pgx/v5"
+)
 
 type UserRepository struct {
 	db *pgx.Conn
 }
 
+func NewUserRepository(db *pgx.Conn) *UserRepository {
+	return &UserRepository{
+		db: db,
+	}
+}
+
+func (r *UserRepository) GetAll(ctx context.Context) ([]models.User, error) {
+	query := `SELECT id, role, fullname, email, password, address, phone, profile_picture FROM users`
+
+	rows, err := r.db.Query(ctx, query)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var users []models.User
+	for rows.Next() {
+		var u models.User
+		err := rows.Scan(&u.Id, &u.Role, &u.Fullname, &u.Email, &u.Password, &u.Address, &u.Phone, &u.ProfilePicture)
+		if err != nil {
+			return nil, err
+		}
+		users = append(users, u)
+
+	}
+	return users, nil
+}
+
+func (r *UserRepository) GetById(ctx context.Context, Id int) (*models.User, error) {
+	query := `SELECT id, role, fullname, email, password, address, phone, profile_picture FROM users WHERE id = $1`
+
+	var u models.User
+
+	err := r.db.QueryRow(ctx, query, Id).Scan(&u.Id, &u.Role, &u.Fullname, &u.Email, &u.Password, &u.Address, &u.Phone, &u.ProfilePicture)
+
+	if err != nil {
+		return nil, err
+	}
+	return &u, nil
+
+}
+
+func (r *UserRepository) GetByEmail(ctx context.Context, Email string) (*models.User, error) {
+	query := `SELECT id, role, fullname, email, password, address, phone, profile_picture FROM users WHERE email = $1`
+
+	var u models.User
+
+	err := r.db.QueryRow(ctx, query, Email).Scan(&u.Id, &u.Role, &u.Fullname, &u.Email, &u.Password, &u.Address, &u.Phone, &u.ProfilePicture)
+
+	if err != nil {
+		return nil, err
+	}
+	return &u, nil
+
+}
+
+func (r *UserRepository) CreateUser(ctx context.Context, )

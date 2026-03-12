085167edc6c8eb8c24f7804dc0f54bbdebaed30e
mslthn <msulthon1909@gmail.com>
2026-03-13 03:29:39 +0700

    feat: create required function in user service

--- internal/service/user.go
+++ internal/service/user.go
@@ -1,6 +1,8 @@
 package service
 
 import (
+	"errors"
+	"main/internal/lib"
 	"main/internal/models"
 	"main/internal/repository"
 )
@@ -16,11 +18,108 @@ func NewUserService(repo *repository.UserRepository) *UserService{
 }
 
 func (s *UserService) GetAll() ([]models.User, error){
-	users, err := s.repo.GetUser()
+	user, err := s.repo.GetUser()
 	if err != nil{
 		return  nil, err
 	}
 	
-	return users, nil
+	return user, nil
 }
 
+func (s *UserService) GetById(id int) (*models.User, error){
+	user, err := s.repo.GetById(id)
+		if err != nil{
+		return  nil, err
+	}
+	
+	return user, nil
+}
+
+func (s *UserService) GetByEmail(email string) (*models.User, error){
+	user, err := s.repo.GetByEmail(email)
+		if err != nil{
+		return  nil, err
+	}
+	
+	return user, nil
+}
+
+func (s *UserService) Register(req *models.CreateUserRequest) error{
+	existingUser, _ := s.repo.GetByEmail(req.Email)
+
+	if existingUser != nil{
+		return  errors.New("Email was registered!")
+	}
+
+	hashed, err := lib.HashPassword(req.Password)
+	if err != nil{
+		return err
+	}
+
+	newUser := models.User{
+		Fullname: req.Fullname,
+		Email: req.Email,
+		Password: hashed,
+	}
+
+	return  s.repo.Create(newUser)
+}
+
+func (s *UserService) Login(req models.LoginUserRequest) (string, error){
+	user, err := s.repo.GetByEmail(req.Email)
+	if err != nil{
+		return "", errors.New("Invalid email/password!")
+	}
+
+	ok := lib.VerifyPassword(req.Password, user.Password)
+	if !ok{
+		return "", errors.New("invalid email/password!")
+	}
+
+	if ok{
+		token, err := lib.GenerateToken(user.Id)
+		
+		if err != nil {
+			return  "", err
+		}
+
+		return  token, nil
+	}
+
+	return  "", err
+}
+
+func (s *UserService) Update(email string, req *models.UpdateUserRequest) (*models.User, error){
+	user, err := s.repo.GetByEmail(email)
+	if  err != nil {
+		return  nil, errors.New("User not found")
+	}
+
+	if req.Fullname != "" {
+		user.Fullname = req.Fullname
+	}
+
+	if  req.Email != "" {
+		user.Email = req.Email
+	}
+
+	if  req.Password != "" {
+		user.Password = req.Password
+	}
+
+	err = s.repo.UpdateUser(*user)
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func (s *UserService) Delete(email string) error{
+	user, err := s.repo.GetByEmail(email)
+	if err != nil{
+		return errors.New("user not found!")
+	}
+
+	return s.repo.DeleteUser(user.Id)
+}

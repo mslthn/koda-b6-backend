22307316e9a047dc178f6a65ddd53425f91170f6
mslthn <msulthon1909@gmail.com>
2026-03-15 17:14:57 +0700

    feat: create product variant model

---
+++ internal/models/product_variant.go
@@ -0,0 +1,20 @@
+package models
+
+type ProductVariant struct {
+	IDVariant   int    `json:"variant_id"`
+	ProductID   int    `json:"product_id"`
+	VariantName string `json:"variant_name"`
+	AddPrice    int    `json:"additional_price"`
+}
+
+type CreateProductVarianRequest struct{
+	ProductID   int    `json:"product_id" binding:"required"`
+	VariantName string `json:"variant_name" binding:"required"`
+	AddPrice    int    `json:"additional_price"`
+}
+
+type UpdateProductVariantRequest struct{
+	ProductID   *int    `json:"product_id"`
+	VariantName *string `json:"variant_name"`
+	AddPrice    *int    `json:"additional_price"`
+}

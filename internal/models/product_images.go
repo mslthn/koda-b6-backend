68cda31ebe0cd0f0dfead9c0854c7fdacd056cf4
mslthn <msulthon1909@gmail.com>
2026-03-15 17:21:54 +0700

    feat: create producvt image model

---
+++ internal/models/product_images.go
@@ -0,0 +1,17 @@
+package models
+
+type ProductImage struct {
+	IDImage   int    `json:"image_id"`
+	ProductID int    `json:"product_id"`
+	Image_url string `json:"image_url"`
+}
+
+type CreateProductImageRequest struct {
+	ProductID int    `json:"product_id" binding:"required"`
+	Image_url string `json:"image_url" binding:"required"`
+}
+
+type UpdateProductImageRequest struct {
+	ProductID *int    `json:"product_id"`
+	Image_url *string `json:"image_url"`
+}

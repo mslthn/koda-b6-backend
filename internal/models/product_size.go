a553ff424e049f157f9f31bbae05b04db89ca972
mslthn <msulthon1909@gmail.com>
2026-03-15 17:14:34 +0700

    feat: create product size model

---
+++ internal/models/product_size.go
@@ -0,0 +1,20 @@
+package models
+
+type ProductSize struct {
+	IDSize    int    `json:"variant_id"`
+	ProductID int    `json:"product_id"`
+	SizeName  string `json:"variant_name"`
+	AddPrice  int    `json:"additional_price"`
+}
+
+type CreateProductSizeRequest struct {
+	ProductID int    `json:"product_id" binding:"required"`
+	SizeName  string `json:"variant_name" binding:"required"`
+	AddPrice  int    `json:"additional_price"`
+}
+
+type UpdateProductSizeRequest struct {
+	ProductID *int    `json:"product_id"`
+	SizeName  *string `json:"variant_name"`
+	AddPrice  *int    `json:"additional_price"`
+}

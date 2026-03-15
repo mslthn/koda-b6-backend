27a43b03177470b52d69370ab7a2bdd495346683
mslthn <msulthon1909@gmail.com>
2026-03-15 17:13:58 +0700

    chore: create product model

--- internal/models/product.go
+++ internal/models/product.go
@@ -5,22 +5,22 @@ type Product struct {
 	Name        string `json:"name"`
 	Desc        string `json:"description"`
 	Price       int    `json:"price"`
-	Quantity    int    `json:"qusntity"`
+	Quantity    int    `json:"quantity"`
 	IsFlashsale bool   `json:"isFlashsale"`
 }
 
-type AddProduct struct {
-	Name        string `json:"name"`
-	Desc        string `json:"description"`
-	Price       int    `json:"price"`
-	Quantity    int    `json:"qusntity"`
+type CreateProductRequest struct {
+	Name        string `json:"name" binding:"required"`
+	Desc        string `json:"description" binding:"required"`
+	Price       int    `json:"price" binding:"required, numeric, min=0"`
+	Quantity    int    `json:"quantity" binding:"required, min=0"`
 	IsFlashsale bool   `json:"isFlashsale"`
 }
 
-type EditProduct struct {
-	Name        string `json:"name"`
-	Desc        string `json:"description"`
-	Price       int    `json:"price"`
-	Quantity    int    `json:"qusntity"`
-	IsFlashsale bool   `json:"isFlashsale"`
+type UpdateProductRequest struct {
+	Name        *string `json:"name"`
+	Desc        *string `json:"description"`
+	Price       *int    `json:"price" binding:"omitempty, numeric, min=0"`
+	Quantity    *int    `json:"quantity" binding:"omitempty, min=0"`
+	IsFlashsale *bool   `json:"isFlashsale"`
 }

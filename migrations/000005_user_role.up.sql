7f001791975cfb742a50fd77b993025ba5b1d6ce
mslthn <msulthon1909@gmail.com>
2026-03-18 00:22:34 +0700

    chore: change user role's table so just containing id and role

--- migrations/000005_user_role.up.sql
+++ migrations/000005_user_role.up.sql
@@ -1,8 +1,4 @@
 CREATE TABLE if not EXISTS "user_role"(
     "id" serial PRIMARY KEY,
-    "role" VARCHAR(80),
-    "user_id" INT NOT NULL,
-    CONSTRAINT fk_user_roles_user
-        FOREIGN KEY ("user_id")
-        REFERENCES "user"("id")
+    "role" VARCHAR(80)
 );

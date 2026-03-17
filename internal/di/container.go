f94a6969db8226ffc73023436bca3a049517ccc9
mslthn <msulthon1909@gmail.com>
2026-03-17 23:21:38 +0700

    feat: create container

--- internal/di/container.go
+++ internal/di/container.go
@@ -1,26 +1,39 @@
 package di
 
 import (
+	"github.com/jackc/pgx/v5"
 	"main/internal/handler"
 	"main/internal/repository"
 	"main/internal/service"
-	"github.com/jackc/pgx/v5"
 )
 
 type Container struct {
+	conn *pgx.Conn
+
+	userRepo    *repository.UserRepository
+	userService *service.UserService
 	userHandler *handler.UserHandler
+
 }
 
 func NewContainer(conn *pgx.Conn) *Container {
-	userRepo := repository.NewUserRepository(conn)
-	userService := service.NewUserService(userRepo)
-	userHandler := handler.NewUserHandler(userService)
-
-	return &Container{
-		userHandler: userHandler,
+	container := Container{
+		conn: conn,
 	}
+
+	container.initDependencies()
+
+	return &container
 }
 
-func (c *Container) UserHandler() *handler.UserHandler {
+func (c *Container) initDependencies() {
+	c.userRepo = repository.NewUserRepository(c.conn)
+	c.userService = service.NewUserService(c.userRepo)
+	c.userHandler = handler.NewUserHandler(c.userService)
+
+
+}
+
+func (c *Container) UserHandler() *handler.UserHandler{
 	return c.userHandler
 }

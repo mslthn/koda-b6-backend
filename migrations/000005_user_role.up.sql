dbe4553ae02cd17248b725fd2aab4786662ea88a
mslthn <msulthon1909@gmail.com>
2026-03-17 23:45:56 +0700

    chore: updating database migration to create user_role table

--- migrations/000005_user_role.up.sql
+++ migrations/000005_user_role.up.sql
@@ -1,8 +1,8 @@
-CREATE table if not EXISTS "user_role"(
-    "id" serial,
+CREATE TABLE if not EXISTS "user_role"(
+    "id" serial PRIMARY KEY,
     "role" VARCHAR(80),
-    "user_id" INT,
-    CONSTRAINT user
+    "user_id" INT NOT NULL,
+    CONSTRAINT fk_user_roles_user
         FOREIGN KEY ("user_id")
         REFERENCES "user"("id")
 );

9ddc3da54df73b56bfb77543958466cc81454ad2
mslthn <msulthon1909@gmail.com>
2026-03-17 15:23:50 +0700

    chore: adding comma on line 5

--- migrations/000002_forgot_password.up.sql
+++ migrations/000002_forgot_password.up.sql
@@ -2,6 +2,6 @@ create table if not exists "forgot_password"(
     "id" serial primary key,
     "email" varchar(50),
     "otp_code" int,
-    "created_at" timestamp default CURRENT_TIMESTAMP
+    "created_at" timestamp default CURRENT_TIMESTAMP,
     "expired_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );

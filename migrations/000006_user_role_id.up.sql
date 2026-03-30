2cb99235ba9899fd7cc63b498a5e2964534ae976
mslthn <msulthon1909@gmail.com>
2026-03-30 16:41:04 +0700

    feat: add foreign key constraint

--- migrations/000006_user_role_id.up.sql
+++ migrations/000006_user_role_id.up.sql
@@ -1,3 +1,9 @@
 alter table users
 add column if not exists role_id INTEGER
 references user_role(id);
+
+alter table users
+add constraint fk_user_role
+foreign key (role_id)
+references user_role(id)
+on delete set NULL;
